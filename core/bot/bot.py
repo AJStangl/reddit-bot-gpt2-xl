1818a631078215729b5e9c27fac81ef021f547ae
AJStangl <ajstangl@gmail.com>
2023-09-16 15:26:23 -0400

    Update bot functionalities and caching system in Reddit Runner
    This commit introduces changes to the Reddit Runner class, especially its primary function and the integration of a caching system. The main function has been updated from 'monitor_comments' to 'run'. The new caching system involves tracking posts to avoid duplicate processing, increasing efficiency. This functionality is enabled by the 'shelve' module added for object persistence. The code also includes error handling features and functionalities to split tokens and handle new submissions. Unnecessary lines of code are also removed to enhance script cleanliness as part of code optimization.

--- core/bot/bot.py
+++ core/bot/bot.py
@@ -1,17 +1,19 @@
-import asyncio
+import json
 import logging
 import os
 import random
-import shelve
-from core.finetune.gather import CaptionProcessor
-import asyncpraw
-from asyncpraw.models import Comment
 import re
 from typing import Optional
 
+import asyncpraw
 import torch
+from asyncpraw.models import Comment
+from asyncpraw.models import Comment
 from dotenv import load_dotenv
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
+from core.finetune.gather import CaptionProcessor
+
 load_dotenv()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -36,7 +38,6 @@ class ModelRunner:
 	def get_encoding(self, text):
 		encoding = self.tokenizer(text, padding=True, return_tensors='pt')
 		return encoding
-
 
 	@torch.no_grad()
 	def run_generation(self, encoding):
@@ -78,23 +79,20 @@ class ModelRunner:
 				new_tokens[match.group(1)] = match.group(2).strip()
 
 			return new_tokens
-		except:
+		except Exception as e:
+			logger.error(e)
 			return None
 
 	@staticmethod
 	def split_token_first_comment(text) -> Optional[str]:
-		try:
-			pattern = re.compile(r'<\|context_level\|>0<\|comment\|>(.+?)<')
-			logger.info(text)
-			matches = re.findall(pattern, text)
-			if len(matches) > 0:
-				return matches[0]
-			else:
-				logger.error("Failed to split first comment text")
-				return None
-		except:
+		pattern = re.compile(r'<\|context_level\|>0<\|comment\|>(.+?)<', re.MULTILINE)
+		logger.info(text)
+		matches = re.findall(pattern, text)
+		if len(matches) == 1:
+			return matches[0]
+		else:
+			logger.error("Failed to split first comment text")
 			return None
-
 
 
 	def load_model_and_tokenizer(self, model_path: str) -> (GPT2Tokenizer, GPT2LMHeadModel):
@@ -105,14 +103,21 @@ class ModelRunner:
 		return tokenizer, model
 
 
-class RedditRunner:
-	caption_processor: CaptionProcessor = CaptionProcessor()
-	bots = os.environ.get("REDDIT_BOTS_TO_REPLY").split(",")
-	cache_path = "cache"
-	os.makedirs(cache_path, exist_ok=True)
+class RedditRunner(object):
+	def __init__(self):
+		self.model_runner: ModelRunner = ModelRunner(os.environ.get("MODEL_PATH"))
+		handle = open(os.environ.get("CONFIG_PATH"), 'r')
+		content = handle.read()
+		handle.close()
+		bot_data = json.loads(content)
+		self.bot_map = {item['name']: item['personality'] for item in bot_data}
+		self.caption_processor: CaptionProcessor = CaptionProcessor()
+		self.cache_path = "cache"
+		os.makedirs(self.cache_path, exist_ok=True)
 
 	async def clear_cache_hourly(self):
 		while True:
+			logger.info("Clearing cache")
 			with shelve.open(str(self.cache_path)) as db:
 				keys_to_remove = []  # List to store keys that need to be removed
 				for key in db.keys():
@@ -152,112 +157,108 @@ class RedditRunner:
 		out = ""
 		for i, r in enumerate(things):
 			out += f"<|context_level|>{i}><|text|>{r}"
-
 		return out
 
-	async def handle_new_submissions(self, subreddit, model_runner, cache_path=cache_path):
+	async def handle_new_submissions(self, subreddit):
+		db = shelve.open(str(self.cache_path))
+		new_reddit = None
 		try:
-			with shelve.open(str(cache_path)) as db:
-				await subreddit.load()
-				async for submission in subreddit.new(limit=10):
-					await submission.load()
-					has_image = False
-					if 'imgur.com' in submission.url or 'i.redd.it' in submission.url:
-						logger.info(f"Submission contains image URL: {submission.url}")
-						has_image = True
+			async for submission in subreddit.stream.submissions():
+				if submission is None:
+					break
+				await submission.load()
+				if 'imgur.com' in submission.url or 'i.redd.it' in submission.url:
+					logger.info(f"Submission contains image URL: {submission.url}")
+					text = self.caption_processor.caption_image_from_url(submission.url)
+				else:
+					logger.info(f"Submission does not contain image URL: {submission.url}")
+					text = submission.selftext
 
+				for bot in self.bot_map.keys():
+					personality = self.bot_map[bot]
+					mapped_submission = {
+						"subreddit": 'r/' + personality,
+						"title": submission.title,
+						"text": text
+					}
+					db[submission.id] = mapped_submission
+					mapped_submission = db.get(submission.id)
 
-
-					if submission.id not in db:
-						text = submission.selftext
-						if has_image:
-							text = self.caption_processor.caption_image_from_url(submission.url)
-
-						mapped_submission = {
-							"subreddit": 'r/' + str(submission.subreddit),
-							"title": submission.title,
-							"text": text
-						}
-						db[submission.id] = mapped_submission
-					else:
-						mapped_submission = db.get(submission.id)
-
-					constructed_string = f"<|startoftext|><|subreddit|>{mapped_submission['subreddit']}<|title|>{mapped_submission['title']}<|text|>{mapped_submission['text']}<|context_level|>0"
-					try:
-						for bot in os.environ["REDDIT_BOTS_TO_REPLY"].split(","):
-							bot_reply_key = f"{bot}_{submission.id}"
-							if bot_reply_key in db:
-								continue
-							result = model_runner.run(constructed_string)
-							tokens: str = ModelRunner.split_token_first_comment(result)
-							new_reddit = asyncpraw.Reddit(site_name=bot)
-							try:
-								submission = await new_reddit.submission(submission.id)
-								reply_text = tokens
-								if reply_text is None:
-									db[bot_reply_key] = True
-									continue
-
-								try:
-									reply = await submission.reply(reply_text)
-								except Exception as e:
-									logger.error(f"Failed to reply, {e}")
-
-								db[bot_reply_key] = True
-								logger.info(f"{bot} has Replied to submission: at https://www.reddit.com{reply.permalink}")
-							except Exception as e:
-								logger.exception(e)
-								db[bot_reply_key] = True
-								logger.error(e)
-							await new_reddit.close()
-					except Exception as e:
-						logger.error(e)
+					constructed_string = f"<|startoftext|><|subreddit|>{mapped_submission['subreddit']}<|title|>{mapped_submission['title']}<|text|>{mapped_submission['text']}<|context_level|>0<|comment|>"
+					bot_reply_key = f"{bot}_{submission.id}"
+					if bot_reply_key in db:
 						continue
+
+					try:
+						new_reddit = asyncpraw.Reddit(site_name=bot)
+						result = self.model_runner.run(constructed_string)
+						submission = await new_reddit.submission(submission.id)
+						reply_text = ModelRunner.split_token_first_comment(result)
+						if reply_text is None:
+							logger.error("Failed to split first comment text")
+							db[bot_reply_key] = True
+							pass
+
+						reply = await submission.reply(reply_text)
+						logger.info(f"{bot} has Replied to submission: at https://www.reddit.com{reply.permalink}")
+						db[bot_reply_key] = True
+					except Exception as e:
+						logger.error(f"Failed to reply, {e}")
+						db[bot_reply_key] = True
+					finally:
+						if new_reddit is None:
+							pass
+						else:
+							await new_reddit.close()
 		finally:
 			db.close()
 
-	async def run(self, model_runner: ModelRunner = ModelRunner(os.environ.get("MODEL_PATH"))):
+	async def send_comment_reply(self, comment: Comment, responding_bot: str, tokens: dict):
+		new_reddit = asyncpraw.Reddit(site_name=responding_bot)
 		try:
-			task = asyncio.create_task(self.clear_cache_hourly())
-			submission_map = {}
-			reddit = asyncpraw.Reddit(site_name=os.environ.get("REDDIT_ACCOUNT_SECTION_NAME"))
-			subreddit = await reddit.subreddit(os.environ.get("SUBREDDIT_TO_MONITOR"))
-			# TODO: If there is no key call `Post` in the cache, then we need to call `Post` to create a submission
+			await comment.load()
+			comment = await new_reddit.comment(comment.id)
+			reply_text = tokens['text']
+			reply = await comment.reply(reply_text)
+			await new_reddit.close()
+			logger.info(
+				f"{responding_bot} has Replied to comment for submission: at https://www.reddit.com{reply.permalink}")
+		except Exception as e:
+			logger.error("I am here!")
+			logger.error(e)
+		finally:
+			await new_reddit.close()
 
-			async for comment in subreddit.stream.comments(skip_existing=True, pause_after=0):
-				if comment is None:
-					logger.info("No new comments, checking for new submissions")
-					await self.handle_new_submissions(subreddit, model_runner)
-					continue
-				submission_id = comment.submission
-				mapped_submission = submission_map.get(submission_id)
-				if mapped_submission is None:
+	async def run(self):
+		asyncio.create_task(self.clear_cache_hourly())
+		while True:
+			try:
+				reddit = asyncpraw.Reddit(site_name=os.environ.get("REDDIT_ACCOUNT_SECTION_NAME"))
+				sub_names = os.environ.get("SUBREDDIT_TO_MONITOR")
+				subreddit = await reddit.subreddit(sub_names)
+				await self.handle_new_submissions(subreddit)
+				async for comment in subreddit.stream.comments(skip_existing=True, pause_after=0):
+					if comment is None:
+						logger.info("No new comments, checking for new submissions")
+						continue
+					submission_id = comment.submission
+
+					responding_bot = random.choice(self.bot_map.keys())
+					personality = self.bot_map[responding_bot]
+
 					submission = await reddit.submission(submission_id)
 					mapped_submission = {
-						"subreddit": 'r' + '/' + str(submission.subreddit),
+						"subreddit": 'r' + '/' + personality,
 						"title": submission.title,
 						"text": submission.selftext
 					}
-					submission_map[submission_id] = mapped_submission
 
-				constructed_string = f"<|startoftext|><|subreddit|>{mapped_submission['subreddit']}<|title|>{mapped_submission['title']}<|text|>{mapped_submission['text']}"
-				constructed_string += await self.construct_context_string(comment)
-				result = model_runner.run(constructed_string)
-				tokens: dict = ModelRunner.split_token(result)
-				responding_bot = random.choice(self.bots)
-				new_reddit = asyncpraw.Reddit(site_name=responding_bot)
-				try:
-					comment = await new_reddit.comment(comment.id)
-					reply_text = tokens['text']
-					reply = await comment.reply(reply_text)
-					logger.info(f"{responding_bot} has Replied to comment for submission: at https://www.reddit.com{reply.permalink}")
-				except Exception as e:
-					logger.error(e)
-					await new_reddit.close()
-					return await self.run()
-				finally:
-					await new_reddit.close()
-		except Exception as e:
-			logger.error(e)
-			return await self.run(model_runner)
-
+					constructed_string = f"<|startoftext|><|subreddit|>{mapped_submission['subreddit']}<|title|>{mapped_submission['title']}<|text|>{mapped_submission['text']}"
+					constructed_string += await self.construct_context_string(comment)
+					result = self.model_runner.run(constructed_string)
+					tokens: dict = ModelRunner.split_token(result)
+					await self.send_comment_reply(comment, responding_bot, tokens)
+			except Exception as e:
+				logger.error("An error has occurring during the primary loop, continuing")
+				logger.error(e)
+				continue
