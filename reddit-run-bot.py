532db244fcea50bb04d2c517539ef1468bbfddb0
AJStangl <ajstangl@gmail.com>
2023-09-20 22:18:13 -0400

    things

--- reddit-run-bot.py
+++ reddit-run-bot.py
@@ -2,20 +2,13 @@ from core.bot.bot import RedditRunner
 
 
 if __name__ == '__main__':
-	import asyncio
-	in_error = False
-	asyncio.run(RedditRunner().run())
-	while True:
-		try:
-			if in_error:
-				print("Restarting...")
-				in_error = False
-			asyncio.run(RedditRunner().run())
-			continue
-		except KeyboardInterrupt:
-			exit(0)
-		except Exception as e:
-			if not in_error:
-				print(f"Error: {e}")
-				in_error = True
-			continue
+    import asyncio
+    try:
+        asyncio.run(RedditRunner().run())
+    except KeyboardInterrupt:
+        exit(0)
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
+
+
